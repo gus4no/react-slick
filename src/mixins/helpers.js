f0efdb16cd7587909dea03368462d3a16acb32a9
Gustavo Robles <gustavo.robles@crowdint.com>
2015-07-29 19:23:26 -0500

    check if slider exists before trying to get offset

--- src/mixins/helpers.js
+++ src/mixins/helpers.js
@@ -39,8 +39,11 @@ var helpers = {
     if (this.props.adaptiveHeight) {
       var selector = '[data-index="' + this.state.currentSlide +'"]';
       if (this.refs.list) {
-        var slickList = this.refs.list.getDOMNode();
-        slickList.style.height = slickList.querySelector(selector).offsetHeight + 'px';
+        var slickList = this.refs.list.getDOMNode(),
+            slide = slickList.querySelector(selector);
+        if(slide){
+          slickList.style.height = slide.offsetHeight + 'px';
+        }
       }
     }
   },
